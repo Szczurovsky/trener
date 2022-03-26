762ed6f3cc25a5272b6198f206a99242c5e1cfc7
Dawid Szczurkowski <dszczurkowski@gmail.com>
2022-03-26 11:21:47 +0100

    pc kulki dzialaja

--- src/components/MainPage/Graph.jsx
+++ src/components/MainPage/Graph.jsx
@@ -27,39 +27,33 @@ const Graph = props => {
     console.log(image, status)
 
     return (
-        //Wersja PC
+
 
         <Page>
-            {console.log(props.width)}
-            {props.width >= 1000 ? <svg width="1200" height="140">
-                <line x1="300" x2="900" y1="80" y2="80" stroke="royalblue" />
-                <circle cx="300" cy="80px" r="40" fill="royalblue" />
-                <Text x="220px" y="140px" scaleToFit="true" width="10px" >lorem ipsum dolor sit amet,ssssssss consectetur adipis</Text>
-                <BiDumbbell fontSize="30px" x="286" y="65" color="white" />
 
-                <circle cx="600" cy="80px" r="40" fill="royalblue" />
-                <BiDumbbell fontSize="30px" x="586" y="65" color="white" />
-                <circle cx="900" cy="80px" r="40" fill="royalblue" />
-                <BiDumbbell fontSize="30px" x="886" y="65" color="white" />
-            </svg> : "prawda"}
-            <Stage width={1200} height={100}>
-                <Layer>
-                    {/* <Line x={300} y={80} points={[0, 0, 600, 0]} fill="royalblue" stroke="royalblue" />
-                    <Circle x={300} y={80} radius={40} fill="royalblue"></Circle>
+            {/* Wersja Big/wide */}
+            {props.width >= 1000 ?
+                (<>
+                    <svg width="1200" height="140">
+                        <line x1="300" x2="900" y1="80" y2="80" stroke="royalblue" />
+                        <circle cx="300" cy="80px" r="40" fill="royalblue" />
+                        <Text x="220px" y="140px" scaleToFit="true" width="10px" >lorem ipsum dolor sit amet,ssssssss consectetur adipis</Text>
+                        <BiDumbbell fontSize="50px" x="276" y="55" color="white" />
 
-                    <Circle x={600} y={80} radius={40} fill="royalblue"></Circle>
-                    <Circle x={900} y={80} radius={40} fill="royalblue"></Circle> */}
+                        <circle cx="600" cy="80px" r="40" fill="royalblue" />
+                        <BiDumbbell fontSize="50px" x="576" y="55" color="white" />
+                        <circle cx="900" cy="80px" r="40" fill="royalblue" />
+                        <BiDumbbell fontSize="50px" x="876" y="55" color="white" />
+                    </svg>
+                    <Stage width={1200} height={100}>
+                        <Layer>
+                            <Text x={250} y={0} text="To jest przykladowy tekst ma sie dzaiala" fill="black" width="100" align="center" />
+                            <Text x={550} y={0} text="To jest przykladowy tekst ma sie dzaiala" fill="black" width="100" align="center" />
+                            <Text x={850} y={0} text="To jest przykladowy tekst ma sie dzaiala" fill="black" width="100" align="center" />
+                        </Layer>
+                    </Stage>
+                </>) : "prawda"}
 
-                    <Text x={250} y={0} text="To jest przykladowy tekst ma sie dzaiala" fill="black" width="100" textAlign="center" />
-                    {console.log(pokaz)}
-                    {/* <Image x={286} y={60} width={50} height={50} image={pokaz} /> */}
-                    {/* <BiDumbbell fontSize="30px" x="886" y="65" color="white" /> */}
-                    {/* <Pokazsie position="relative">
-                        <BiDumbbell top="100" />
-                    </Pokazsie> */}
-
-                </Layer>
-            </Stage>
             napisz sie
 
         </Page>
